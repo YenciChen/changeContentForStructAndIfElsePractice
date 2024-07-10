21aac12a6870a19f445c5b8c47e050b0e6c010e5
Yenci <a278862002@gmail.com>
2024-07-10 17:26:54 +0800

    Initial Commit

---
+++ changeContentForStructAndIfElsePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  changeContentForStructAndIfElsePractice
+//
+//  Created by 陳妍慈 on 2024/7/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
